91bee49b0672e7d4e4b960b90dc6adbd761b34da
aliceliudev <zliu5962@conestogac.on.ca>
2021-03-12 11:59:35 -0500

    Update caculator.js

---
+++ Caculator/caculator.js
@@ -0,0 +1,17 @@
+let _operators;
+let _numbers;
+let _display;
+
+
+window.onload = function() {
+
+const_operators = document.querySelectorAll('.button-op');
+
+}
+
+var buttonPress =function(event) {
+
+    console.log(event.target.getAttribute('data-num');)
+
+    _display.value = event
+}
