2a0aee1271d7a2cc0ef9e133a5d89c04879824a8
aliceliudev <zliu5962@conestogac.on.ca>
2021-03-15 23:31:36 -0400

    draft

--- Caculator/caculator.js
+++ Caculator/caculator.js
@@ -1,17 +1,34 @@
 let _operators;
 let _numbers;
 let _display;
+let isOperator = false;
 
 
 window.onload = function() {
+    _operators = document.querySelectorAll('.button-op');   //Store all the operators in variable
+    _numbers = document.querySelectorAll('.button-num');
+    _display = document.querySelector('#output');
+    // _display.value = eval("1+1");
+    _button = document.querySelector('.button-num');
+    _button.onclick = buttonPress;
 
-const_operators = document.querySelectorAll('.button-op');
 
-}
 
-var buttonPress =function(event) {
 
-    console.log(event.target.getAttribute('data-num');)
+};
 
-    _display.value = event
-}
+var numbuttonPress = function(event) {
+    //console.log(event.target.getAttribute('data-num'));
+
+    _display.value += event.target.getAttribute('data-num');
+};
+
+var opButton = function(event) {
+    if (isOperator) return;
+    else {
+        _display.value += event.target.getAttribute('data-op');
+    }
+
+
+
+};
