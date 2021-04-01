5193e770840ea1937d428f335f1311a14d96c53b
aliceliudev <zliu5962@conestogac.on.ca>
2021-04-01 08:20:42 -0400

    new assignment
    new

--- a7/speakers/speakers.js
+++ a7/speakers/speakers.js
@@ -1,3 +1,19 @@
+let file;
+
 $(document).ready(function() {
-	
+    $("#nav_list a").click(function() {
+        const title = this.getAttribute('title');
+        const url = "json_files/" + title + ".json";
+        $.getJSON(url, function(data) {
+            const speaker = data.speakers[0];
+            let html = "<h1>" + speaker.title + "</h1>";
+            html += "<img src =" + speaker.image + ">";
+            html += "<h2>" + speaker.month + "</h2>";
+            html += "<p>" + speaker.text + "</p>";
+            $('main').html(html);
+
+        });
+    });
+
 }); // end ready
+
